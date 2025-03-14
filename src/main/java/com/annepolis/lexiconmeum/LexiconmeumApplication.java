979eb6526405a73aa1b9e501263dcec6f5b317bb
annedyne <50835753+annedyne@users.noreply.github.com>
2025-03-14 16:13:33 -0400

    First commit of project

---
+++ src/main/java/com/annepolis/lexiconmeum/LexiconmeumApplication.java
@@ -0,0 +1,13 @@
+package com.annepolis.lexiconmeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexiconmeumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LexiconmeumApplication.class, args);
+    }
+
+}
