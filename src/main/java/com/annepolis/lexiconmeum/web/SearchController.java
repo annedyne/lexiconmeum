1b5ec530e9dd5822f72248a53dc8ceb2f50ec9a0
Anne Smith <annepolis@gmail.com>
2025-04-27 18:35:25 -0400

    add lexicon json parsing: Includes: Added SearchProperties file to get config values out of code

--- src/main/java/com/annepolis/lexiconmeum/web/SearchController.java
+++ src/main/java/com/annepolis/lexiconmeum/web/SearchController.java
@@ -1,6 +1,7 @@
 package com.annepolis.lexiconmeum.web;
 
 import com.annepolis.lexiconmeum.textsearch.TextSearchComponent;
+import com.annepolis.lexiconmeum.textsearch.TextSearchProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,18 +14,29 @@ import java.util.List;
 public class SearchController {
 
     private final TextSearchComponent textSearchComponent;
+    private final TextSearchProperties searchProperties;
 
-    public SearchController(TextSearchComponent textSearchComponent){
+    public SearchController(TextSearchComponent textSearchComponent, TextSearchProperties searchProperties ){
         this.textSearchComponent = textSearchComponent;
+        this.searchProperties = searchProperties;
     }
 
     @GetMapping("/prefix")
-    public List<String> searchByPrefix(@RequestParam String prefix, @RequestParam(defaultValue = "10") int limit) {
-        return textSearchComponent.getWordsStartingWith(prefix, limit);
+    public List<String> searchByPrefix(@RequestParam String prefix, @RequestParam int limit) {
+
+        return textSearchComponent.getWordsStartingWith(prefix, getEffectiveLimit(limit));
     }
 
     @GetMapping("/suffix")
-    public List<String> searchBySuffix(@RequestParam String suffix, @RequestParam(defaultValue = "10") int limit) {
-        return textSearchComponent.getWordsEndingWith(suffix, limit);
+    public List<String> searchBySuffix(@RequestParam String suffix, @RequestParam int limit) {
+        return textSearchComponent.getWordsEndingWith(suffix, getEffectiveLimit(limit));
     }
+
+    private int getEffectiveLimit(int limit){
+        return (limit == 0 || limit > searchProperties.getDefaultLimit())
+                ? searchProperties.getDefaultLimit()
+                : limit;
+    }
+
+
 }
