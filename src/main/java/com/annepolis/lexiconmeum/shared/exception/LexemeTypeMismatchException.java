c02e90e5103e13613d12815858b4c248c6097031
Anne Smith <50835753+annedyne@users.noreply.github.com>
2025-07-21 19:06:59 -0400

    fix(core): add missing file from previous commit 92f89a6a
    add missing file to avoid runtime breakage

---
+++ src/main/java/com/annepolis/lexiconmeum/shared/exception/LexemeTypeMismatchException.java
@@ -0,0 +1,7 @@
+package com.annepolis.lexiconmeum.shared.exception;
+
+public class LexemeTypeMismatchException extends RuntimeException {
+    public LexemeTypeMismatchException(String message) {
+        super(message);
+    }
+}
