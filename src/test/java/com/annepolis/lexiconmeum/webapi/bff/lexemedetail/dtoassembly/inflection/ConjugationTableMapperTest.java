b730caffa2e6ffc899a37d1ee6cbf785a15c69d7
Anne Smith <50835753+annedyne@users.noreply.github.com>
2025-10-20 18:38:53 -0400

    test(grammar): add tests for verb form ordering d - Verify forms are ordered by number and person in ascending order.

--- src/test/java/com/annepolis/lexiconmeum/webapi/bff/lexemedetail/dtoassembly/inflection/ConjugationTableMapperTest.java
+++ src/test/java/com/annepolis/lexiconmeum/webapi/bff/lexemedetail/dtoassembly/inflection/ConjugationTableMapperTest.java
@@ -6,12 +6,14 @@ import com.annepolis.lexiconmeum.shared.model.grammar.GrammaticalMood;
 import com.annepolis.lexiconmeum.shared.model.grammar.GrammaticalVoice;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConjugationTableMapperTest {
 
     @Test
-    void toConjugationTablesMappedInOrder(){
+    void toConjugationTablesMoodsMappedInOrder(){
         ConjugationTableMapper mapper = new ConjugationTableMapper();
         Lexeme lexeme = TestUtil.getNewTestVerbLexeme();
         ConjugationGroupDTO groupDTO = mapper.toInflectionTableDTO(lexeme);
@@ -21,6 +23,22 @@ class ConjugationTableMapperTest {
         assertEquals(GrammaticalMood.INDICATIVE.getHistoricalName(), groupDTO.getConjugationTableDTOList().get(3).getMood());
         assertEquals(GrammaticalVoice.PASSIVE.name(), groupDTO.getConjugationTableDTOList().get(3).getVoice());
     }
+
+    @Test
+    void forms_inNumberAndPerson_Ascending(){
+        ConjugationTableMapper mapper = new ConjugationTableMapper();
+        Lexeme lexeme = TestUtil.getNewTestVerbLexeme();
+        ConjugationGroupDTO groupDTO = mapper.toInflectionTableDTO(lexeme);
+
+        List<ConjugationTableDTO> tableDTOS =  groupDTO.getConjugationTableDTOList();
+        assertEquals("amō", tableDTOS.get(0).getTenses().get(0).getForms().get(0));
+        assertEquals("amās", tableDTOS.get(0).getTenses().get(0).getForms().get(1));
+        assertEquals("amat", tableDTOS.get(0).getTenses().get(0).getForms().get(2));
+        assertEquals("amāmus", tableDTOS.get(0).getTenses().get(0).getForms().get(3));
+        assertEquals("amātis", tableDTOS.get(0).getTenses().get(0).getForms().get(4));
+        assertEquals("amant", tableDTOS.get(0).getTenses().get(0).getForms().get(5));
+    }
+
     @Test
     void toConjugationTableMapsAllFormsInGivenLexemeTense(){
         ConjugationTableMapper mapper = new ConjugationTableMapper();
