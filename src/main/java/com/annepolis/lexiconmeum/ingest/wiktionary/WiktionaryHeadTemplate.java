ac84048641129d205edfd8539cfdc222506e1ddc
Anne Smith <50835753+annedyne@users.noreply.github.com>
2026-01-16 19:46:01 -0500

    feat(parser): introduce `POSParserKey` enum for part-of-speech and mapping POS to template_head name
    Will be used to allow parser registry look-up based on POS AND template head name combination.

--- src/main/java/com/annepolis/lexiconmeum/ingest/wiktionary/WiktionaryHeadTemplate.java
+++ src/main/java/com/annepolis/lexiconmeum/ingest/wiktionary/WiktionaryHeadTemplate.java
@@ -3,7 +3,11 @@ package com.annepolis.lexiconmeum.ingest.wiktionary;
 
 public enum WiktionaryHeadTemplate {
     VERB("la-verb"),
-    PARTICIPLE("la-part");
+    PARTICIPLE("la-part"),
+    ADJECTIVE_POSITIVE("la-adj"),
+    ADJECTIVE_COMPARATIVE("la-adj-comp"),
+    ADJECTIVE_SUPERLATIVE("la-adj-sup");
+
 
     private final String name ;
 
